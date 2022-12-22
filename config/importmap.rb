1d5f898903b18fe74f5e1d0ab7e215da3b5d5ca3
Philippe DIOLLOT <philippediollot@hotmail.fr>
2022-12-22 18:49:47 +0100

    adding comments

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,19 +6,12 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "allo", to: "jquery/allo.js"
-pin "allo2", to: "bootstrap/js/allo2.js"
+# pin "allo", to: "jquery/allo.js"
+# pin "allo2", to: "bootstrap/js/allo2.js"
 
-pin "jquery", to: "jquery/jquery.min.js"
-pin "bootstrap", to: "bootstrap/js/bootstrap.bundle.min.js"
+pin "jquery", to: "jquery/jquery.min.js" #to avoid relative file names, add path in app/assets/config/manifest.js
+pin "bootstrap", to: "bootstrap/js/bootstrap.bundle.min.js" #to avoid relative file names, add path in app/assets/config/manifest.js
 pin "owl-carousel", to: "owl-carousel.js"
 pin "animation", to: "animation.js"
 pin "imagesloaded", to: "imagesloaded.js"
 pin "custom", to: "custom.js"
-
-# <script src="vendor/jquery/jquery.min.js"></script>
-# <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-# <script src="assets/js/owl-carousel.js"></script>
-# <script src="assets/js/animation.js"></script>
-# <script src="assets/js/imagesloaded.js"></script>
-# <script src="assets/js/custom.js"></script>
