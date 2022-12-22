ff83fc241aa24038b96771aa2ecc304bdf2bd7b0
Philippe DIOLLOT <philippediollot@hotmail.fr>
2022-12-22 18:47:36 +0100

    vendor folder ok, animation.js doesn't work and need to try with vendor folder from template directly

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
+
 //= link_tree ../../../vendor/assets/javascripts .js
-//= link_tree ../../../lib/assets/jquery .js
 //= link_tree ../../../vendor/assets/images .jpg
 //= link_tree ../../../vendor/assets/images .png
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,12 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-// import "../../vendor/assets/javascripts/jquery.min.js"
-// import "../../lib/bootstrap/js/bootstrap.bundle.min.js"
+import "allo";
+import "allo2";
+import "jquery"
+import "bootstrap"
+import "owl-carousel"
+// import "animation"
+import "imagesloaded"
+import "custom"
 
-// import "../../vendor/assets/javascripts/owl-carousel"
-// import "../../vendor/assets/javascripts/animation"
-// import "../../vendor/assets/javascripts/imagesloaded"
-// import "../../vendor/assets/javascripts/custom"
