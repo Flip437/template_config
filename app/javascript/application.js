06eedad5e69848c0c864c8abdffd448fbe69fab4
Philippe DIOLLOT <philippediollot@hotmail.fr>
2022-12-22 17:10:54 +0100

    css ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// import "../../vendor/assets/javascripts/jquery.min.js"
+// import "../../lib/bootstrap/js/bootstrap.bundle.min.js"
+
+// import "../../vendor/assets/javascripts/owl-carousel"
+// import "../../vendor/assets/javascripts/animation"
+// import "../../vendor/assets/javascripts/imagesloaded"
+// import "../../vendor/assets/javascripts/custom"
